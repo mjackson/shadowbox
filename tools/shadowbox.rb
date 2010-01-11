86809288bfe616583458e26eee66a64199e5af88
Michael Jackson <mjijackson@gmail.com>
2010-01-11 13:35:59 -0700

    Removed Shadowbox::Builder class in favor of Rake

--- tools/shadowbox.rb
+++ tools/shadowbox.rb
@@ -1,9 +1,5 @@
-require 'fileutils'
-require 'optparse'
-require 'erb'
-
 module Shadowbox
-  @source_dir = File.expand_path(File.dirname(__FILE__) + '/../source-new')
+  @source_dir = File.expand_path(File.dirname(__FILE__) + '/../source')
 
   # get the current version of the code from the source
   @current_version = File.open(@source_dir + '/core.js') do |f|
@@ -50,128 +46,16 @@ module Shadowbox
                  css.gsub!(/^\s+/, '')
                  css.gsub!(/(,|:)\s+/, '\1')
                  css.gsub!(/\s+\{/, '{')
+                 css.gsub!(/(\{|;).*?(\S)/m, '\1\2')
                  css
                else
                  raise ArgumentError
                end
 
       if outfile
-        File.open(outfile, 'w') {|f| f.print result }
+        File.open(outfile, 'w') {|f| f << result }
       else
         $stdout.puts result
-      end
-    end
-  end
-
-  class Builder
-    NOTICE = %q{
-    This directory contains a custom build of Shadowbox, and contains only a subset
-    of its available features. All files necessary to run Shadowbox are combined into
-    as few as possible in order to maximize efficiency in load and execution time.
-
-    This particular build includes support for the following features:
-
-    Language: <%=@language%>
-    Adapter:  <%=@adapter%>
-    Players:  <%=@players.join(', ')%>
-
-    <%="Support for CSS selectors is also included via Sizzle.js <http://sizzlejs.com/>." if @use_sizzle%>
-
-    For more information, please visit the Shadowbox website at http://shadowbox-js.com/.
-    }.gsub(/^    /, '')
-
-    attr_reader :errors
-    attr_accessor :adapter, :language, :players, :use_sizzle, :use_swfobject, :target, :overwrite
-
-    def initialize
-      @errors        = []
-      @version       = Shadowbox.current_version
-      @adapter       = Shadowbox.default_adapter
-      @language      = Shadowbox.default_language
-      @players       = Shadowbox.default_players
-      @use_sizzle    = false
-      @use_swfobject = false # will be set automatically if needed
-      @target        = "build"
-      @overwrite     = true
-    end
-
-    def notice
-      template = ERB.new(NOTICE)
-      template.result(binding)
-    end
-
-    def source(*args)
-      File.join(Shadowbox.source_dir, *args)
-    end
-
-    def target(*args)
-      File.join(@target, *args)
-    end
-
-    def run
-      @errors = []
-
-      # include swfobject if swf or flv players are being used
-      @players.uniq!
-      @use_swfobject = @players.include?('swf') || @players.include?('flv')
-
-      @errors << %{Invalid adapter: #{@adapter}} unless Shadowbox.valid_adapter?(@adapter)
-      @errors << %{Invalid language: #{@language}} unless Shadowbox.valid_language?(@language)
-      invalid_players = @players.reject {|player| Shadowbox.valid_player?(player) }
-      @errors << %{Invalid player(s): #{invalid_players.join(',')}} if invalid_players.any?
-
-      make_target unless @errors.any?
-
-      # stop here if there are any errors
-      return false if @errors.any?
-
-      # create javascript file list
-      jsfiles = []
-      jsfiles << "shadowbox.js"
-      jsfiles << ["adapters", "shadowbox-#{@adapter}.js"]
-      jsfiles << ["languages", "shadowbox-#{@language}.js"]
-      jsfiles += @players.map {|player| ["players", "shadowbox-#{player}.js"] }
-      jsfiles << ["libraries", "sizzle", "sizzle.js"] if @use_sizzle
-      jsfiles << ["libraries", "swfobject", "swfobject.js"] if @use_swfobject
-
-      # compile js
-      js = jsfiles.map {|paths| File.read(source(*paths)) }
-      js << %<Shadowbox.options.players=["#{@players.join('","')}"];>
-      js << %<Shadowbox.options.useSizzle=#{@use_sizzle};>
-      File.open(target("shadowbox.js"), 'w') {|f| f.print js.join("\n") }
-
-      # copy all other assets
-      FileUtils.cp source("shadowbox.css"), target("shadowbox.css")
-      FileUtils.mkdir target("assets")
-      Dir[source("assets", "*.png")].each do |file|
-        FileUtils.cp file, target("assets")
-      end
-      if @use_sizzle || @use_swfobject
-        FileUtils.mkdir target("libraries")
-        FileUtils.cp_r source("libraries", "sizzle"), target("libraries") if @use_sizzle
-        FileUtils.cp_r source("libraries", "swfobject"), target("libraries") if @use_swfobject
-      end
-
-      FileUtils.cp source("..", "README"), target
-      FileUtils.cp source("..", "LICENSE"), target
-      File.open(target("BUILD"), 'w') {|f| f.print notice }
-
-      true
-    end
-
-    def make_target
-      target_dir = File.dirname(@target)
-      if !File.writable?(target_dir)
-        @errors << %{Target directory (#{target_dir}) is not writable}
-      elsif File.exist?(@target)
-        if @overwrite
-          FileUtils.rm_rf(@target)
-          FileUtils.mkdir_p(@target)
-        else
-          @errors << %{Target directory (#{@target}) already exists}
-        end
-      else
-        FileUtils.mkdir_p(@target)
       end
     end
   end
