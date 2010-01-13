9a9fdd7f053f232a385cfe2ee2c526b1f8c610ea
Michael Jackson <mjijackson@gmail.com>
2010-01-13 09:48:38 -0700

    added test for Shadowbox.path

--- tests/core.js
+++ tests/core.js
@@ -7,3 +7,8 @@ test("Basic Requirements", function() {
     ok(document.getElementsByTagName, "getElementsByTagName");
     ok(RegExp, "RegExp");
 });
+
+test("Script Path", function() {
+    var scriptPath = document.location.href.replace(/index\.html$/, 'build/');
+    ok(Shadowbox.path == scriptPath, "Script path was determined correctly");
+});
