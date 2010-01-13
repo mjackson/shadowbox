73a9ba7950a2f5247e89c489d3b7701cbbdba276
Michael Jackson <mjijackson@gmail.com>
2010-01-13 09:49:11 -0700

    added tests for setup/teardown

--- tests/cache.js
+++ tests/cache.js
@@ -2,46 +2,51 @@ module("cache");
 
 test("Select/Find", function() {
     var sel = Shadowbox.select();
-
-    same(sel.length, 1, 'Automatically select all links with rel="shadowbox"');
-
-    var anchor1 = document.getElementById('anchor1'),
-        anchor2 = document.getElementById('anchor2');
-
+    ok(sel.length == 1, 'Automatically select all links with rel="shadowbox"');
+    var anchor1 = document.getElementById('anchor1');
+    var anchor2 = document.getElementById('anchor2');
     var sel = Shadowbox.select(anchor1);
-
-    same(sel.length, 1, 'Select one element when given explicitly');
-
+    ok(sel.length == 1, 'Select one element when given explicitly');
     var sel = Shadowbox.select([anchor1, anchor2]);
-
-    same(sel.length, 2, 'Select multiple elements when given explicitly');
-    same(sel[0], anchor1, 'Keep references to original elements');
+    ok(sel.length == 2, 'Select multiple elements when given explicitly');
+    ok(sel[0] == anchor1, 'Keep references to original elements');
 });
 
-test("Automatic Setup", function() {
+test("Automatic Setup and Teardown", function() {
+    Shadowbox.setup();
     var anchor1 = document.getElementById("anchor1");
-
-    // should already be there from call to Shadowbox.init
     var cacheObj = Shadowbox.getCache(anchor1);
-
-    ok(cacheObj, 'Cache contains automatically setup object');
-    same(cacheObj.link, anchor1, "Cache object keeps reference to original link element");
-    same(cacheObj.title, "Snow Leopard", "Cache object pulls title from link element");
+    ok(cacheObj, "Cache contains automatically setup object");
 });
 
-test("Manual Setup", function() {
+test("Manual Setup and Teardown", function() {
     var anchor2 = document.getElementById("anchor2");
-
-    same(Shadowbox.getCache(anchor2), false, "Cache miss for new link object");
-
+    ok(Shadowbox.getCache(anchor2) == false, "Cache miss for new link object");
     Shadowbox.setup(anchor2);
-
     var cacheObj = Shadowbox.getCache(anchor2);
-
     ok(cacheObj, "Cache contains manually setup object");
-    same(cacheObj.title, null, "Cache object has empty title");
+});
 
+test("Cache Object", function() {
+    var anchor1 = document.getElementById("anchor1");
+    var cacheObj = Shadowbox.getCache(anchor1);
+    ok(cacheObj.link == anchor1, "Cache object keeps reference to original link element");
+    ok(cacheObj.title == "Snow Leopard", "Cache object pulls title from link element");
+    ok(cacheObj.options.animate === false, "Inline boolean option is parsed correctly");
+    ok(cacheObj.options.animSequence === 'hw', "Inline string option is parsed correctly");
+    var anchor2 = document.getElementById("anchor2");
+    var cacheObj = Shadowbox.getCache(anchor2);
+    ok(cacheObj.title == null, "Cache object has empty title");
+});
+
+test("Automatic Teardown", function() {
+    var anchor1 = document.getElementById("anchor1");
+    Shadowbox.teardown();
+    ok(Shadowbox.getCache(anchor1) == false, "Object successfully removed from cache");
+});
+
+test("Manual Teardown", function() {
+    var anchor2 = document.getElementById("anchor2");
     Shadowbox.teardown(anchor2);
-
-    equals(Shadowbox.getCache(anchor2), false, "Cache miss for removed object");
+    ok(Shadowbox.getCache(anchor2) == false, "Object successfully removed from cache");
 });
