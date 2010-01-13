2434a10dc825cb8022f389489ea092e218a7c120
Michael Jackson <mjijackson@gmail.com>
2010-01-13 11:36:47 -0700

    edited documentation

--- source/intro.js
+++ source/intro.js
@@ -1,5 +1,5 @@
 /*!
- * Shadowbox.js JavaScript library, version @VERSION
+ * Shadowbox JavaScript library, version @VERSION
  * http://shadowbox-js.com/
  *
  * Copyright 2007-2010, Michael J. I. Jackson
